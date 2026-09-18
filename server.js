ee095b195c7927cdeb8e34a0a74cd3853471cb32
Maria Robin <mariarobin2021@gmail.com>
2026-09-18 19:43:49 +0530

    Create uploads directory automatically

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const multer = require("multer");
-
+const fs = require("fs");
 const app = express();
-
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+}
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/");
